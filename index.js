8487e6ff944b49d0d95db2929f5fc16863797722
Kristina Peru <krissy.germanov@gmail.com>
2022-02-03 13:20:52 -0700

    commit tree 13a5b58cebb4d108f611e46f60f5849dc13e1fe3

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'frank';
+
+function firstFunc() {
+    return customerName;
+}
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+  }
+function setBestCustomer(){
+    bestCustomer = 'not bob';
+}
+ function overwriteBestCustomer() {
+     bestCustomer = 'maybe bob';
+ }
+ function changeLeastFavoriteCustomer() {
+     leastFavoriteCustomer = 'maybe frank';
+ }
